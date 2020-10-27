2f6155bb5a51e9aae08be46803397f4acb1bbf75
JacobBarnard <jacob.barnard21@gmail.com>
2020-10-27 15:33:31 -0600

    10.1 10/27 database, /movies

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const db = require('./dbConnectExec.js')
 const app = express();
 
 app.get("/hi", (req, res)=> {
@@ -9,6 +9,21 @@ app.get("/hi", (req, res)=> {
 // app.post()
 // app.put()
 // app.delete()
+
+app.get("/movies", (req, res) => {
+    //get data from database
+    db.executeQuery(
+        `SELECT * from movie 
+        left join Genre 
+        on Genre.GenrePK = Movie.GenreFK`)
+        .then( (result) => {
+            res.status(200).send(result)
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send()
+        })
+})
 
 app.listen(5000, () => {
     console.log("app is running on port 5000")
